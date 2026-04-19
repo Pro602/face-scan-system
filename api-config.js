f9c02b156e38f62997690885359b1bd0fe1aa789
Pro602 <Weera54440@gmail.com>
2026-04-19 14:31:49 +0700

    Update api-config.js

--- api-config.js
+++ api-config.js
@@ -2,4 +2,4 @@
 //  ตั้งค่า Google Apps Script Web App URL
 //  แก้ไข URL ด้านล่างหากมีการ Deploy GAS ใหม่
 // ============================================================
-const GAS_API_URL = "https://script.google.com/macros/s/AKfycby63Gpq4d0zY7umM-ZCHLzHLjBkbj05fgNnDqNfE7pCc4uQ1QibreHfdamF3cOou0y7tw/exec"
+const GAS_API_URL = "https://script.google.com/macros/s/AKfycbxC2Qf4d-vH0r39AziEIiJ0i8zaARhQeo0gyR0GUSzk3A0u7URPcp1YTgktvwaRQBAfYw/exec"
